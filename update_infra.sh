db6ef950f10383785e4777363365d4032b68546f
Eduard <edu@MacBook-MacBook-Pro-de-Eduard.local>
2018-11-23 18:03:28 +0100

    Minor fix

--- update_infra.sh
+++ update_infra.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# update-stack of initial bucket containing the CloudFormation files of the infrastructure.
-aws cloudformation update-stack \
- --stack-name bucket \
- --capabilities CAPABILITY_NAMED_IAM \
- --template-body file://infrastructure/bucket.yaml \
- --region eu-west-1 
-
-
  aws s3 sync ./ s3://rakuten-infrastructure --acl bucket-owner-full-control --acl public-read --exclude='.*'
 
  aws cloudformation update-stack \
